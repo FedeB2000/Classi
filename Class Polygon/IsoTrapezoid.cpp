0cc4274f5f43e4228fcaf2c9d20cc3afbc1743b9
FedeB2000 <fedebelli002@gmail.com>
2023-05-16 13:08:26 +0200

    miglioria della classe

--- Class Polygon/IsoTrapezoid.cpp
+++ Class Polygon/IsoTrapezoid.cpp
@@ -2,10 +2,64 @@
 
 IsoTrapezoid::IsoTrapezoid()
 {
-
+ bottomSide=0;
+ height=0;
+ topSide=0;
 }
-
+IsoTrapezoid::IsoTrapezoid(float bottomSide,float height,float topSide)
+{
+ bottomSide=bottomSide;
+ height=height;
+ topSide=topSide;
+ CheckbottomSide();
+ Checkpropieties();
+}
+bool IsoTrapezoid::CheckbottomSide()
+{
+if(bottomSide<topSide)
+{
+    return false;
+}
+else 
+{
+    return true;
+}
+}
+ bool IsoTrapezoid::Checkpropieties()
+ {
+    if(bottomSide<0 && topSide<0&&height<0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+ }
+float IsoTrapezoid::GetSide()
+{
+    float side = sqrt((pow(bottomSide-topSide)+pow(height)));
+  return side;
+}
 IsoTrapezoid::~IsoTrapezoid()
 {
-
+cout << " IsoTrapezoid- destructor" << endl;
+}
+IsoTrapezoid::IsoTrapezoid(const IsoTrapezoid& other) 
+{
+    bottomSide= other.bottomSide;
+    topSide = other.topSide;
+    height = other.height;
+}
+float IsoTrapezoid::Area()
+{
+   return ((bottomSide+topSide)*height/2);
+}
+float IsoTrapezoid::Perimeter()
+{
+ return ((topSide+bottomSide)+height*2);
+}
+void IsoTrapezoid::Draw()
+{
+    cout << "Ho costruito isotrapezoide" << endl;
 }
--- Class Polygon/IsoTrapezoid.h
+++ Class Polygon/IsoTrapezoid.h
@@ -1,16 +1,27 @@
 #ifndef ISOTRAPEZOID_H
-#define ISOTRAPEZOID_H
-
+#define ISOTRAPEZOID_
+# include "polygon.h"
 #pragma once
 
-class IsoTrapezoid
+class IsoTrapezoid : public Polygon
 {
 public:
     IsoTrapezoid();
+    IsoTrapezoid(float bottomSide,float height,float topSide);
+    IsoTrapezoid(const IsoTrapezoid& other);
     ~IsoTrapezoid();
+    float GetSide();
+    bool CheckbottomSide();
+    bool Checkpropieties();
+    void Draw();
+    float Area();
+    float Perimeter();
 
-private:
 
+ private:
+      float bottomSide;
+      float height;
+      float topSide;
 };
 
 #endif
